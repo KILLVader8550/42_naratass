73edd719a3889ac3df7f02c969e78f9dc278f87c
KILLVader8550 <145247735+KILLVader8550@users.noreply.github.com>
2025-09-02 12:01:41 +0700

    update func name

--- libft/libft.h
+++ libft/libft.h
@@ -10,8 +10,6 @@ void    *ft_memmove(void *dst, const void *src, size_t len);
 void    *ft_memchr(const void *s, int c, size_t n);
 void    *ft_calloc(size_t count, size_t size);
 
-char    *ft_tolower(char *str);
-char    *ft_toupper(char *str);
 char    *ft_strchr(const char *s, int c);
 char    *ft_strrchr(const char *s, int c);
 char    *ft_strnstr(const char *haystack, const char *needle, size_t len);
@@ -23,7 +21,9 @@ int		ft_isalpha(int c);
 int		ft_isascii(int c);
 int     ft_isdigit(int c);
 int		ft_isprint(int c);
-int     ft_strncmp(char *s1, char *s2, size_t n);
+int		ft_tolower(int c);
+int		ft_toupper(int c);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
 int     ft_memcmp(const void *s1, const void *s2, size_t n);
 
 size_t  ft_strlcpy(char *dest, const char *src, size_t destsize);
