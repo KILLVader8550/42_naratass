dd6e1ece87bcbc0d0dfba6c1b05a593421559972
KILLVader8550 <145247735+KILLVader8550@users.noreply.github.com>
2025-09-02 12:03:45 +0700

    update function name

--- libft/libft.h
+++ libft/libft.h
@@ -3,7 +3,7 @@
 # include <stdlib.h>
 # include <unistd.h>
 
-void    *ft_bzero(void *s, size_t n);
+void    ft_bzero(void *s, size_t n);
 void    *ft_memcpy(void *restrict dst, const void *restrict src, size_t n);
 void    *ft_memset(void *b, int c, size_t len);
 void    *ft_memmove(void *dst, const void *src, size_t len);
@@ -27,7 +27,7 @@ int		ft_strncmp(const char *s1, const char *s2, size_t n);
 int     ft_memcmp(const void *s1, const void *s2, size_t n);
 
 size_t  ft_strlcpy(char *dest, const char *src, size_t destsize);
-size_t  strlcat(char * restrict dst, const char * restrict src, size_t dstsize);
+size_t	ft_strlcat(char *dst, const char *src, size_t size);
 size_t  ft_strlen(const char *str);
 
 char    *ft_substr(char const *s, size_t start, size_t len);
