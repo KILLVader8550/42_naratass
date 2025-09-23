6a39b69f5764bce4de41226a5e8e470d2f1b954b
KILLVader8550 <145247735+KILLVader8550@users.noreply.github.com>
2025-09-23 18:23:41 +0700

    resize function

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -4,50 +4,63 @@
 #include <fcntl.h>
 #define BUFFER_SIZE 3
 
+void	putstr(int *fd, char *str, char *stash, int *ptr, int *j)
+{
+	int	bytes_read;
+
+	while ((bytes_read = read(*fd, stash, BUFFER_SIZE)) > 0)
+	{
+		stash[bytes_read] = '\0';
+		*j = 0;
+		while (stash[*j] != '\0')
+		{
+			if (stash[*j] == '\n')
+			{
+				str[(*ptr)] = '\0';
+				(*j)++;
+				return ;
+			}
+			str[(*ptr)++] = stash[(*j)++];
+		}
+		str[(*ptr)] = '\0';
+	}
+}
+
+void	get_previous_stash(char *str, char *stash, int *ptr, int *j)
+{
+	if (stash && str)
+	{
+		while (stash[*j])
+			str[(*ptr)++] = stash[(*j)++];
+	}
+}
+
 char	*get_next_line(int fd)
 {
 	static char	*stash;
 	static int	j;
 	char		*str;
-	int			bytes_read;
 	int			ptr;
 
 	str = (char *) malloc((sizeof(char) * 100));
 	if (!str)
 		return (NULL);
 	ptr = 0;
-	if (stash)
-	{
-		while (stash[j])
-		{
-			str[ptr++] = stash[j++];
-		}
-	}
-	stash = (char *) malloc((sizeof(char) * (BUFFER_SIZE + 1)));
+	get_previous_stash(str, stash, &ptr, &j);
 	if (!stash)
-		return (NULL);
-	while ((bytes_read = read(fd, stash, BUFFER_SIZE)) > 0)
 	{
-		stash[bytes_read] = '\0';
-		j = 0;
-		while (stash[j] != '\0')
-		{
-			if (stash[j] == '\n')
-			{
-				str[ptr] = '\0';
-				j++;
-				return (str);
-			}
-			str[ptr++] = stash[j++];
-		}
+		stash = (char *) malloc((sizeof(char) * (BUFFER_SIZE + 1)));
+		if (!stash)
+			return (NULL);
 	}
-	str[ptr] = '\0';
+	putstr(&fd, str, stash, &ptr, &j);
 	return (str);
 }
 
 int main()
 {
 	int fd = open("text.txt", O_RDONLY);
+	// int fd = 1;
 	char	*str;
 
 	str = get_next_line(fd);
